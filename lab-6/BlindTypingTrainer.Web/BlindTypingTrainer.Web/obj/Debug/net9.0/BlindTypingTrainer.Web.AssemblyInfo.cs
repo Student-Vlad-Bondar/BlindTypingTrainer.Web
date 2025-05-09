c628704d00811b79e12d68adaa24d39e731609aa
VladBondar <ipz235_bvv@student.ztu.edu.ua>
2025-05-09 22:26:22 +0300

    feat(live-feedback): move JS into typing-feedback.js; implement real-time highlighting and timer

--- lab-6/BlindTypingTrainer.Web/BlindTypingTrainer.Web/obj/Debug/net9.0/BlindTypingTrainer.Web.AssemblyInfo.cs
+++ lab-6/BlindTypingTrainer.Web/BlindTypingTrainer.Web/obj/Debug/net9.0/BlindTypingTrainer.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlindTypingTrainer.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a651ab4431a8d3496f581ad0507f4a91b0fe7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+313bce368181d5a925f05d3c2988df49c5027c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlindTypingTrainer.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlindTypingTrainer.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
